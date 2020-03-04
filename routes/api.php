a17ca569ba5619a3670ae3c051c108f287a6233b
Mohammed Zayan <zayanit@gmail.com>
2020-03-04 23:22:24 +0300

    Basic routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,3 +11,20 @@ use Illuminate\Http\Request;
 |
 */
 
+// Buyers
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+// Categories
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create', 'edit']]);
+
+// Products
+Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+// Sellers
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+// Transactions
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+// Users
+Route::resource('users', 'User\UserController', ['except' => ['create', 'edit']]);
