39adf3bef60ff5ef2ccfa2d9caf679a7645248f2
Mohammed Zayan <zayanit@gmail.com>
2020-03-18 21:04:29 +0300

    Remove temp vars from seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -23,17 +23,14 @@ class DatabaseSeeder extends Seeder
         Transaction::truncate();
         DB::table('category_product')->truncate();
 
-        $usersQuantity = 200;
-        $categoriesQuantity = 30;
-        $productsQuantity = 1000;
-        $transactionsQuantity = 1000;
-
-        factory(User::class, $usersQuantity)->create();
-        factory(Category::class, $categoriesQuantity)->create();
-        factory(Product::class, $productsQuantity)->create()->each(function($product){
-            $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
-            $product->categories()->attach($categories);
-        });
-        factory(Transaction::class, $transactionsQuantity)->create();
+        factory(User::class, 200)->create();
+        factory(Category::class, 30)->create();
+        factory(Product::class, 1000)->create()->each(
+            function ($product) {
+                $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+                $product->categories()->attach($categories);
+            }
+        );
+        factory(Transaction::class, 1000)->create();
     }
 }
